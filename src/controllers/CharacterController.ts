3b4860415b5f349d39b007cf8adeb561f7c759c6
elugo-arkus <elugo@arkusnexus.com>
2025-09-18 17:46:37 -0600

    Small refactor

--- src/controllers/CharacterController.ts
+++ src/controllers/CharacterController.ts
@@ -1,63 +1,42 @@
 import { CharacterApi } from "~/services";
 import { makeAutoObservable } from 'mobx';
 import { Character } from "~/types";
-// export class CharacterController {
-//   constructor(private characterModel: CharacterModel) {}
-
-//   async fetchCharacters() {
-//     this.characterModel.setLoading(true);
-//     this.characterModel.setError(null);
-    
-//     try {
-//       const api = new CharacterApi();
-//       const fetchedCharacters = await api.fetchRickAndMortyCharactersAPI();
-//       this.characterModel.setCharacters(fetchedCharacters);
-//       this.characterModel.setLoading(false);
-//     } catch (error) {
-//       this.characterModel.setError(error instanceof Error ? error.message : 'Failed to fetch characters');
-//       this.characterModel.setLoading(false);
-//     }
-//   }
-// }
-
 
 class CharacterController {
-  isLoading: boolean = false;
-  characters: Character[] = [];
-  isError: boolean = false;
+  _characters: Character[] = [];
+  _isError: boolean = false;
 
   constructor() {
     makeAutoObservable(this);
   }
 
-  get getIsLoading () {
-    return this.isLoading
+  get characters () {
+    return this._characters;
   }
 
-  get getcharacters () {
-    return this.characters;
+  set characters (characters: Character[]) {
+    this._characters = characters;
   }
 
-  get getIsError () {
-    return this.isError;
+  set isError (isError: boolean) {
+    this._isError = isError;
   }
 
-  setIsLoading (isLoading: boolean) {
-    this.isLoading = isLoading;
+  get isError () {
+    return this._isError;
   }
 
-  async fetchCharacters() {
-      this.setIsLoading(true);
+  fetchCharacters = async () => {
       try {
+        
         const api = new CharacterApi();
         const fetchedCharacters = await api.fetchRickAndMortyCharactersAPI();
         this.characters = fetchedCharacters;
-        this.setIsLoading(false);
+     
       } catch (error) {
-        this.setIsLoading(false);
         this.isError = true;
       }
-  }
+  };
 }
 
 const characterController = new CharacterController();
