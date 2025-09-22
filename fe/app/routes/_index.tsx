a4f3f6416e94f08ff10006aa02281b830a88d661
elugo-arkus <elugo@arkusnexus.com>
2025-09-22 17:24:06 -0600

    Register form added

--- fe/app/routes/_index.tsx
+++ fe/app/routes/_index.tsx
@@ -1,12 +1,16 @@
 import LoginView from "../views/LoginView";
 import { authController } from "../controllers/AuthController";
 import { useNavigate } from "@remix-run/react";
-
+import { useEffect } from "react";
+ 
 export default function Index() {
   const navigate = useNavigate();
-  if (!authController.isAuthenticated) {
-    navigate('/characters');
-  }
-  
+ 
+  useEffect(() => {
+    if (authController.isAuthenticated) {
+      navigate("/characters");
+    }
+  }, [navigate]);
+ 
   return <LoginView />;
 }
--- fe/app/views/index.ts
+++ fe/app/views/index.ts
@@ -1,2 +1,3 @@
 export * from './LoginView';
 export * from './CharactersView';
+export { default as RegisterView } from './RegisterView';
